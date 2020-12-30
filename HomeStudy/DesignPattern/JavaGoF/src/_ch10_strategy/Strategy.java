5baa4c3584920a7939f2fd0b8760a66fdeb24464
RobiLee <dev4robi@gmail.com>
2020-12-30 20:25:12 +0900

    10-11ch 스터디.

---
+++ HomeStudy/DesignPattern/JavaGoF/src/_ch10_strategy/Strategy.java
@@ -0,0 +1,7 @@
+package _ch10_strategy;
+
+public interface Strategy {
+    
+    public abstract Hand nextHand();
+    public abstract void study(boolean win);
+}
---
+++ HomeStudy/DesignPattern/JavaGoF/src/_ch11_composite/FileTreatmentException.java
@@ -0,0 +1,12 @@
+package _ch11_composite;
+
+public class FileTreatmentException extends Exception {
+
+    public FileTreatmentException() {
+
+    }
+
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}
