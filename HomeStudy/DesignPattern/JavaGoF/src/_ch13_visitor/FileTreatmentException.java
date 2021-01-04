83865aece056f445b84097f7325007be64682ea9
RobiLee <dev4robi@gmail.com>
2021-01-04 20:55:34 +0900

    Ch12-14.

---
+++ HomeStudy/DesignPattern/JavaGoF/src/_ch13_visitor/FileTreatmentException.java
@@ -0,0 +1,12 @@
+package _ch13_visitor;
+
+public class FileTreatmentException extends RuntimeException {
+    
+    public FileTreatmentException() {
+
+    }
+
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}
