37464afefe6530d38e2cedf19470d67ff2a6f6c4
RobiLee <dev4robi@gmail.com>
2021-01-11 21:47:21 +0900

    ch14

---
+++ HomeStudy/DesignPattern/JavaGoF/src/_ch14_chain_of_responsibility/NoSupport.java
@@ -0,0 +1,13 @@
+package _ch14_chain_of_responsibility;
+
+public class NoSupport extends Support {
+
+    public NoSupport(String name) {
+        super(name);
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        return false;
+    }
+}
