6f673429f13fcd6cf53c869d0672ac5048731278
RobiLee <dev4robi@gmail.com>
2021-04-28 21:49:07 +0900

    JPA Study. 연관관계 맵핑.

---
+++ HomeStudy/Java/JPA/kyh-jpa-basic/ex1-hello-jpa/src/main/java/relations/Team.java
@@ -0,0 +1,29 @@
+package relations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+public class Team {
+    
+    @Id @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
+}
