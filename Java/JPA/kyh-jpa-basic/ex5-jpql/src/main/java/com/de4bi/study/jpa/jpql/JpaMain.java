90aa10298249c3a8b917de5e7bbdc8a8bcf45edd
TaeHoonLee <robi02@naver.com>
2021-05-10 22:58:51 +0900

    JPA JPQL Study.

--- Java/JPA/kyh-jpa-basic/ex5-jpql/src/main/java/com/de4bi/study/jpa/jpql/JpaMain.java
+++ Java/JPA/kyh-jpa-basic/ex5-jpql/src/main/java/com/de4bi/study/jpa/jpql/JpaMain.java
@@ -14,8 +14,16 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import com.de4bi.study.jpa.jpql.domain.Address;
 import com.de4bi.study.jpa.jpql.domain.Member;
+import com.de4bi.study.jpa.jpql.domain.Order;
+import com.de4bi.study.jpa.jpql.domain.Product;
+import com.de4bi.study.jpa.jpql.domain.Team;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -106,9 +114,153 @@ public class JpaMain {
     }
 
     /**
-     * 
+     * 프로젝션: SELECT절 조회 대상을 지정.
      */
     public static void test4(EntityManager em) {
+        // 테스트 초기화
+        Product p = new Product();
+        p.setName("Product1");
+        p.setPrice(1000);
+        p.setStockAmount(40);
+        em.persist(p);
+
+        Address addr = new Address();
+        addr.setCity("city");
+        addr.setStreet("street");
+        addr.setZipcode("zipcode");
+
+        Order o = new Order();
+        o.setAddress(addr);
+        o.setOrderAmount(30);
+        o.setProduct(p);
+        em.persist(o);
+
+        Team t = new Team();
+        t.setName("T1");
+        em.persist(t);
+
+        Member m = new Member();
+        m.setName("M1");
+        m.setAge(10);
+        m.setTeam(t);
+        em.persist(m);
+        em.flush();
+        em.clear();
+
+        // 엔티티 프로젝션
+        List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+        Member findMember = result.get(0);
+        findMember.setAge(20); // -> EntityManager에서 관리되므로 tx.commit시점에 값이 변경됨.
+        
+        // 엔티티 프로젝션: 조인
+        // 위 쿼리보다는 아래 쿼리가 조금 더 안정적(DB쿼리랑 비슷한게 좋다.)
+        String q1 = "select m.team from Member m";          // 묵시적 Join
+        String q2 = "select t from Member m join m.team t"; // 명시적 Join
+        List<Team> resultTeam = em.createQuery(q2, Team.class).getResultList();
+        Team findTeam = resultTeam.get(0);
+        log.info("findTeam.name = " + findTeam.getName());
+
+        // 임베디드 타입 프로젝션
+        List<Address> resultAddr = em.createQuery("select o.address from Order o", Address.class).getResultList();
+        log.info("resultAddr.city = " + resultAddr.get(0).getCity());
+        
+        // 스칼라 타입 프로젝션
+        // 1) 제너릭을 사용하여 조회
+        List<Object[]> resultScalar = em.createQuery("select m.name, m.age from Member m", Object[].class).getResultList();
+        Object[] resultAry = resultScalar.get(0);
+        log.info("member.name = " + resultAry[0]); // m.name
+        log.info("member.age = " + resultAry[1]); // m.age
+        
+        // 2) new 명령어로 조회
+        List<MemberDto> resultScalar2 = em.createQuery("select new com.de4bi.study.jpa.jpql.JpaMain$MemberDto(m.name, m.age) from Member m", MemberDto.class).getResultList();
+        for (MemberDto dto : resultScalar2) {
+            log.info("dto.name = " + dto.getName());
+            log.info("dto.age = " + dto.getAge());
+        }
+    }
+
+    @AllArgsConstructor @Getter @Setter // 조회쿼리와 순서/타입이 일치하는 생성자가 필요.
+    public static class MemberDto {
+        private String name;
+        private int age;
+    };
+
+    /**
+     * 페이징.
+     * 다음 두 API로 페이징을 추상화.
+     *  1) setFirstResult(int startPosition) : 조회 시작 위치 (0부터)
+     *  2) setMaxResults(int maxResult) : 조회할 데이터 수
+     */
+    public static void test5(EntityManager em) {
+        // 테스트 초기화
+        for (int i = 0; i < 100; ++i) {
+            Member m = new Member();
+            m.setName("M" + i);
+            m.setAge((i + 1) * 2);
+            m.setTeam(null);
+            em.persist(m);
+        }
+
+        em.flush();
+        em.clear();
+
+        List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+            .setFirstResult(10)
+            .setMaxResults(20)
+            .getResultList();
+
+        log.info("ReulstSize = " + result.size());
+        for (Member m : result) {
+            log.info("m.id = " + m.getId() + " / m.name = " + m.getName() + " / m.age = " + m.getAge());
+        }
+    }
+
+    /**
+     * 조인.
+     */
+    public static void test6(EntityManager em) {
+        // 테스트 초기화
+        Team t = new Team();
+        t.setName("T");
+        em.persist(t);
+
+        for (int i = 0; i < 5; ++i) {
+            Member m = new Member();
+            m.setName("M" + i);
+            m.setAge((i + 1) * 2);
+            m.setTeam(t);
+            em.persist(m);
+        }
+
+        String query = "select m from Member m inner join m.team t";
+        // String query = "select m from Member m left outer join m.team t";
+        List<Member> result = em.createQuery(query, Member.class).getResultList();
+        // String thetaJoinQuery = "select m from Member m, Team t where m.name = t.name";
+        // List<Member> result = em.createQuery(thetaJoinQuery, Member.class).getResultList();
+
+        for (Member m : result) {
+            log.info("m.name = " + m.getName() + " / m.team.name = " + m.getTeam().getName());                        
+        }
+
+        // ON 절을 사용한 조인 (JPA2.1+ / Hibernate5.1+)
+        String qy = "select m from Member m left join m.team t on t.name = 'T'";
+        List<Member> rst = em.createQuery(query, Member.class).getResultList();
+
+        for (Member m : rst) {
+            log.info("m.name = " + m.getName() + " / m.team.name = " + m.getTeam().getName());
+        }
+
+        // 현재, 서브쿼리는 JPQL에서 지원하지 않음. (2021.05.10)
+        // 조인으로 풀 수 있으면 풀어서 해결... 또는 쿼리를 2번 쓰는 방식을 사용
+        // JPA 표준은 WHERE, HAVING 절에서만 서브쿼리를 지원하지만,
+        // 하이버네이트에서 select절도 지원한다.
+    }
+
+    /**
+     *
+     */
+    public static void test7(EntityManager em) {
+
 
     }
 
@@ -121,14 +273,16 @@ public class JpaMain {
 
             // test1(em);
             // test2(em);
-            test3(em);
+            // test3(em);
             // test4(em);
-            
+            // test5(em);
+            // test6(em);
+            test7(em);
 
             tx.commit();
         }
         catch (Exception e) {
-            log.error("{}", e);
+            log.error("Exception!", e);
             tx.rollback();
         }
 
