31d54a8613876813fe59de648c2c139e82a5d621
RobiLee <dev4robi@gmail.com>
2021-05-20 20:26:14 +0900

    REST-API Study Initial Commit.

---
+++ Java/Spring/bks_rest_api/rest-api-with-spring/src/main/java/com/de4bi/study/restapiwithspring/RestApiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.de4bi.study.restapiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithSpringApplication.class, args);
+	}
+
+}
---
+++ Java/Spring/bks_rest_api/rest-api-with-spring/src/test/java/com/de4bi/study/restapiwithspring/RestApiWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.de4bi.study.restapiwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
