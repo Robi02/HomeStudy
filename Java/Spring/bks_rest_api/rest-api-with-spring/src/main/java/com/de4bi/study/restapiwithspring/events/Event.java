b4ad765c92889c614a3d41560602e07512bce434
RobiLee <dev4robi@gmail.com>
2021-05-20 20:46:27 +0900

    Event domain.

---
+++ Java/Spring/bks_rest_api/rest-api-with-spring/src/main/java/com/de4bi/study/restapiwithspring/events/Event.java
@@ -0,0 +1,30 @@
+package com.de4bi.study.restapiwithspring.events;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = {"id"}) // 개체간의 연관관계가 상호참조가 되버리면 StackOverflow가 발생하기에 id만 사용 권장
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional) 값이 없는 경우 온라인 모임
+    private int basePrice; // (optional)
+    private int maxPrice; // (optional)
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+}
---
+++ Java/Spring/bks_rest_api/rest-api-with-spring/src/main/java/com/de4bi/study/restapiwithspring/events/EventStatus.java
@@ -0,0 +1,6 @@
+package com.de4bi.study.restapiwithspring.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
