a896e726f4df78e9ae8079669be604e910b6932c
RobiLee <dev4robi@gmail.com>
2021-06-17 19:48:25 +0900

    FilterInvocationMetadataSource 추가.

--- Java/Spring/jsw_core_spring_security/core-spring-security-board/src/main/java/com/de4bi/study/security/corespringsecurityboard/security/configs/SecurityConfig.java
+++ Java/Spring/jsw_core_spring_security/core-spring-security-board/src/main/java/com/de4bi/study/security/corespringsecurityboard/security/configs/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.de4bi.study.security.corespringsecurityboard.security.configs;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import com.de4bi.study.security.corespringsecurityboard.security.UserRoles;
@@ -7,12 +11,17 @@ import com.de4bi.study.security.corespringsecurityboard.security.filter.AjaxLogi
 import com.de4bi.study.security.corespringsecurityboard.security.handler.CustomAccessDeniedHandler;
 import com.de4bi.study.security.corespringsecurityboard.security.handler.CustomAuthenticationFailureHandler;
 import com.de4bi.study.security.corespringsecurityboard.security.handler.CustomAuthenticationSuccessHandler;
+import com.de4bi.study.security.corespringsecurityboard.security.metadatasource.UrlFilterInvocationSecurityMetadataSource;
 import com.de4bi.study.security.corespringsecurityboard.security.provider.CustomAuthenticationProvider;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -24,6 +33,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 import lombok.RequiredArgsConstructor;
 
@@ -63,9 +73,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return handler;
     }
 
+    @Bean
+    public FilterSecurityInterceptor customFilterSecurityInterceptor() throws Exception {
+        FilterSecurityInterceptor filterSecurityInterceptor = new FilterSecurityInterceptor();
+        filterSecurityInterceptor.setSecurityMetadataSource(urlFilterInvocationSecurityMetadataSource());
+        filterSecurityInterceptor.setAccessDecisionManager(affirmativeBased());
+        filterSecurityInterceptor.setAuthenticationManager(authenticationManagerBean());
+        return filterSecurityInterceptor;
+    }
+
+    private UrlFilterInvocationSecurityMetadataSource urlFilterInvocationSecurityMetadataSource() {
+        return new UrlFilterInvocationSecurityMetadataSource();
+    }
+
+    private AccessDecisionManager affirmativeBased() {
+        AffirmativeBased affirmativeBased = new AffirmativeBased(getAccessDecistionVoters());
+        return affirmativeBased;
+    }
+
+    private List<AccessDecisionVoter<?>> getAccessDecistionVoters() {
+        return Arrays.asList(new RoleVoter());
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        /* [1. 메모리 방식의 사용자 인증]
+        /* [1-1. 메모리 방식의 사용자 인증]
         // String password = passwordEncoder().encode("1111");
 
         // auth.inMemoryAuthentication().withUser("user").password(password).roles(UserRoles.USER.name());
@@ -73,24 +105,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // auth.inMemoryAuthentication().withUser("admin").password(password).roles(UserRoles.ADMIN.name());
         */
 
-        // [2. DB를 연동한 사용자 인증 (내부에서 accountRepository를 사용)]
+        // [1-2. DB를 연동한 사용자 인증 (내부에서 accountRepository를 사용)]
         // auth.userDetailsService(userDetailsService);
 
-        // [3. 사용자 정의 인증 Provider 등록 (내부에서 userDetailsService, passwordEncoder를 사용)]
+        // [1-3. 사용자 정의 인증 Provider 등록 (내부에서 userDetailsService, passwordEncoder를 사용)]
         // (주의: 1, 2, 3 방식을 동시에 사용하면, Provider가 여러개가 등록되게 되고 그 중 하나만 통과하면 인증이 성공하는 것으로 보인다!)
         auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            .antMatchers("/", "/users", "/user/login/**", "/login*").permitAll()
-            .antMatchers("/mypage").hasAuthority(UserRoles.USER.name())
-            .antMatchers("/messages").hasAuthority(UserRoles.MANAGER.name())
-            .antMatchers("/config").hasAuthority(UserRoles.ADMIN.name())
-            .anyRequest().authenticated()
-        ;
+        // [2-1] HttpSecurity로 인가처리
+        // http
+        //     .authorizeRequests()
+        //     .antMatchers("/", "/users", "/user/login/**", "/login*").permitAll()
+        //     .antMatchers("/mypage").hasAuthority(UserRoles.USER.name())
+        //     .antMatchers("/messages").hasAuthority(UserRoles.MANAGER.name())
+        //     .antMatchers("/config").hasAuthority(UserRoles.ADMIN.name())
+        //     .anyRequest().authenticated()
+        // ;
 
         http
             .formLogin()
@@ -105,6 +138,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
             .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
+        ;
+
+        // [2] FilterSecurityInterceptor + FilterInvocationSecurityMetadataSocurce로 인가처리
+        http
+            .addFilterBefore(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class)
         ;
     }
 
---
+++ Java/Spring/jsw_core_spring_security/core-spring-security-board/src/main/java/com/de4bi/study/security/corespringsecurityboard/security/metadatasource/UrlFilterInvocationSecurityMetadataSource.java
@@ -0,0 +1,69 @@
+package com.de4bi.study.security.corespringsecurityboard.security.metadatasource;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+/**
+ * FilterSecurityInterceptor(권한정보 조회)
+ * ->
+ * FilterInvocationSecurityMetadataSource(요청정보와 매칭되는 권한정보 추출 -> 각 RequestMatcher(경로)별로 requestMap에 저장)
+ * ->
+ * 권한목록이 존재하지 않으면 -> 인가처리 안함
+ * ->
+ * 존재하면 호출: decide(Authentication, FilterInvocation, List<ConfigAttribute>)
+ * ->
+ * AccessDecisionManager 호출
+ */
+public class UrlFilterInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
+
+    private LinkedHashMap<RequestMatcher, List<ConfigAttribute>> requestMap = new LinkedHashMap<>();
+
+    @Override
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+        
+        HttpServletRequest request = ((FilterInvocation) object).getRequest();
+
+        requestMap.put(new AntPathRequestMatcher("/mypage"), Arrays.asList(new SecurityConfig("ROLE_USER")));
+
+        if (requestMap != null) {
+            for (Map.Entry<RequestMatcher, List<ConfigAttribute>> entry : requestMap.entrySet()) {
+                RequestMatcher matcher = entry.getKey();
+                if (matcher.matches(request)) {
+                    return entry.getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        Set<ConfigAttribute> allAttributes = new HashSet<>();
+
+        for (Map.Entry<RequestMatcher, List<ConfigAttribute>> entry : requestMap.entrySet()) {
+            allAttributes.addAll(entry.getValue());
+        }
+
+        return allAttributes;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return FilterInvocation.class.isAssignableFrom(clazz);
+    }
+}
