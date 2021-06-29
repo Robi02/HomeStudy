89d419e1e8dd10a368c374c6fecef6a2bac6437b
RobiLee <dev4robi@gmail.com>
2021-06-29 21:41:20 +0900

    Initial Commit.

---
+++ Java/Spring/kyh-spring-jpa/jpashop/src/test/java/com/de4bi/study/jpa/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.de4bi.study.jpa.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
