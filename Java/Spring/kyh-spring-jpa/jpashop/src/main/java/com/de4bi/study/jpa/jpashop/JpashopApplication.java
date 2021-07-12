a7baf2c1956682e07cdee652baa6f0e4489ef652
TaeHoonLee <robi02@naver.com>
2021-07-12 23:04:57 +0900

    JPA + H2설정 및 테스트.

--- Java/Spring/kyh-spring-jpa/jpashop/src/main/java/com/de4bi/study/jpa/jpashop/JpashopApplication.java
+++ Java/Spring/kyh-spring-jpa/jpashop/src/main/java/com/de4bi/study/jpa/jpashop/JpashopApplication.java
@@ -8,8 +8,6 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 
-		
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
---
+++ Java/Spring/kyh-spring-jpa/jpashop/src/test/java/com/de4bi/study/jpa/jpashop/MemberRepositoryTests.java
@@ -0,0 +1,35 @@
+package com.de4bi.study.jpa.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional // @Test 어노테이션이 있는 경우 rollback을 수행함
+    // @Rollback(false) // 이 어노테이션을 키면 @Test환경에서도 rollback을 수행하지 않음
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+        
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
